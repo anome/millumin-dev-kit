2bf4949fcb02f1bcf9cd2efc0a047cc5993daeaa
Philippe Chaurand <contact@anomes.com>
2013-06-13 09:11:43 +0200

    New : Python example

---
+++ Python/Python2Millumin.py
@@ -0,0 +1,68 @@
+import OSC
+import time, random, threading
+
+
+
+
+# TO SEND OSC MESSAGES
+oscClient = OSC.OSCClient()
+oscClient.connect(  ('127.0.0.1',5000)  )
+
+
+
+
+# TO RECEIVE OSC MESSAGES
+oscServer = OSC.OSCServer(  ('127.0.0.1',5001)  )
+
+def printing_handler(addr, tags, stuff, source):
+    print "---"
+    print "received new osc msg from %s" % OSC.getUrlStr(source)
+    print "with addr : %s" % addr
+    print "typetags %s" % tags
+    print "data %s" % stuff
+    print "---"
+
+oscServer.addMsgHandler("/millumin/layer/opacity/0", printing_handler)
+oscServer.addMsgHandler("/millumin/layer/opacity/1", printing_handler)
+
+st = threading.Thread( target = oscServer.serve_forever )
+st.start()
+
+
+
+
+
+
+
+
+# TEST 1 : SENDING OSC BUNDLE
+bundle = OSC.OSCBundle()
+msg = OSC.OSCMessage()
+msg.setAddress("/millumin/layer/opacity/0")
+msg.append(80)
+bundle.append(msg)
+bundle.append( {'addr':"/millumin/layer/opacity/1", 'args':[80]} )
+oscClient.send(bundle)
+
+
+
+
+
+
+# TEST 2 : SENDING AND RECEIVING OSC MESSAGES
+try :
+	seed = random.Random()
+	while 1 :
+		msg = OSC.OSCMessage()
+		msg.setAddress("/millumin/layer/opacity/0")
+		n = seed.randint(0, 100)
+		msg.append(n)
+		oscClient.send(msg)
+		time.sleep(1)
+except KeyboardInterrupt :
+	print "\nClosing OSCClient and OSCServer"
+	oscClient.close()
+	oscServer.close()
+	st.join()
+	print "Done"
+        
